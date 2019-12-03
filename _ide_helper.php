890125357e2a8cd05aa0cbce49864a3664c6da8d
vahed-mrv <vahed.mrv@gmail.com>
2019-12-03 11:13:15 +0330

    ReGenerate ide helper.

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.3.0 on 2019-10-23 12:57:46.
+ * Generated for Laravel 6.3.0 on 2019-12-03 07:42:30.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
